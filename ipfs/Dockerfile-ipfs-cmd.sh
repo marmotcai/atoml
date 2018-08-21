94d02e65315ed29c8d878e131068175f62aa593d
marmotcai <caijun@atoml.com>
2018-08-21 18:09:32 +0800

    modify Dockerfile-ipfs-cmd Dockerfile

--- ipfs/Dockerfile-ipfs-cmd.sh
+++ ipfs/Dockerfile-ipfs-cmd.sh
@@ -1,3 +1,8 @@
 #!bin/bash
 
 docker build . -f ./Dockerfile-ipfs-cmd -t atoml/ipfs-cmd
+
+docker run --rm -p 5001:5001 -p 8080:8080 atoml/ipfs-cmd
+
+# docker rm -f my-ipfs-cmd
+# docker run --name my-ipfs-cmd -p 5001:5001 -p 8080:8080 -d atoml/ipfs-cmd
