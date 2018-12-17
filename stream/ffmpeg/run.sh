1525482e2e361c04b06685eff15013320b99a6c5
root <root@local.cvideo.pro>
2018-12-17 14:03:54 +0800

    ffmpeg update

---
+++ stream/ffmpeg/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run --rm=true \
+         -v ~/input:/input -v ~/output:/output \
+        atoml/ffmpeg \
+        -stats -y \
+        -i /input/sample.mp4 \
+        -c:v libx265 -pix_fmt yuv420p10 \
+        -t 5 -f mp4 /output/test.mp4
